af9e5eae7fa4c8146a82ce19720411dced860d9c
Mugisha <habiyaadolphe19@gmail.com>
2024-04-23 00:51:01 +0200

    Add sticky navigation

--- js/script.js
+++ js/script.js
@@ -54,7 +54,28 @@ allLinks.forEach((link) => {
   })
 })
 
+///////////////////////////////////////////////////////////
+//  Sticky Navigation
 
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  console.log(ent);
+  if (!ent.isIntersecting)
+    document.body.classList.add('sticky')
+
+  if (ent.isIntersecting)
+  document.body.classList.remove('sticky')
+},
+ {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+
+obs.observe(sectionHeroEl);
 
 
 ///////////////////////////////////////////////////////////
